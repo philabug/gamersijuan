6da7d49dfec7d51167b4baf0ceb1ad8a0dd3c238
philabug <philabug@gmail.com>
2020-10-10 02:18:57 +0800

    5th commit

---
+++ static/js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $(document).mouseup(function (e) { 
+        if ($(e.target).closest("#search-content").length 
+                    === 0) { 
+            $("#result").hide(); 
+        } 
+    }); 
+
+    ///////////////// fixed menu on scroll for desktop
+    if ($(window).width() > 992) {
+        $(window).scroll(function(){  
+        if ($(this).scrollTop() > 40) {
+            $('#navbar_top').addClass("fixed-top");
+            // add padding top to show content behind navbar
+            $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+            }else{
+            $('#navbar_top').removeClass("fixed-top");
+            // remove padding top from body
+            $('body').css('padding-top', '0');
+            }   
+        });
+    } //end if
+
+})
